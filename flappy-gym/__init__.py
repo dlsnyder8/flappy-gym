b11656e46c5b98ff42449cf2700e1f712f1ee116
dlsnyder8 <51924672+dlsnyder8@users.noreply.github.com>
2019-06-28 10:23:33 -0400

    Update __init__.py

--- flappy-gym/__init__.py
+++ flappy-gym/__init__.py
@@ -1,1 +1,10 @@
-import logging
+from gym.envs.registration import register
+
+register(
+    id='foo-v0',
+    entry_point='gym_foo.envs:FooEnv',
+)
+register(
+    id='foo-extrahard-v0',
+    entry_point='gym_foo.envs:FooExtraHardEnv',
+)
