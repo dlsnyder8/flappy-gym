cc35990a89844bfc38c2da6f0619261b132d586b
khemetrobinson <51961633+khemetrobinson@users.noreply.github.com>
2019-07-10 14:15:37 -0400

    Add files via upload

---
+++ flappy-gym/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='flappy-gym',
+      version='0.0.1',
+      install_requires=['gym', 'pygame', 'numpy', 'random', 'time']  # And any other dependencies foo needs
+)
