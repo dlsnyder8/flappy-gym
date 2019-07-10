f9817f0d0598135dfc435a58483a0c8624308701
khemetrobinson <51961633+khemetrobinson@users.noreply.github.com>
2019-07-10 14:22:37 -0400

    Delete setup.py

--- flappy-gym/envs/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-setup(name='flappy-gym',
-      version='0.0.1',
-      install_requires=['gym', 'pygame', 'numpy', 'random', 'time']  # And any other dependencies foo needs
-)
