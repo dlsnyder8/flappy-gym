78a3b572052b98ea1bb0b756f18077fde540441a
khemetrobinson <khemetmrobinson@gmail.com>
2019-07-11 14:37:10 -0400

    directories

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from setuptools import setup
 setup(name='flappy_gym',
       version='0.0.1',
       install_requires=['gym', 'pygame', 'numpy', 'random', 'time']  # And any other dependencies foo needs
-)
+      )
