2bef793cb64c63c84a0dc3fefb0540f42200c6db
khemetrobinson <khemetmrobinson@gmail.com>
2019-07-15 12:46:52 -0400

    updating q-table

--- ai.py
+++ ai.py
@@ -1,9 +1,5 @@
 import gym
-from gym import error, spaces, utils
-from gym.utils import seeding
-import random
 import numpy as np
-from random import shuffle
 from flappy_gym.envs.flappy_env import FlappyEnv
 from flappy_gym import envs
 
@@ -11,48 +7,40 @@ env = gym.make('flappygym-v0')
 env.reset()
 
 
-LEARNINGRATE = 0.1
+LEARNING_RATE = 0.1
 DISCOUNT = 0.95
 EPISODES = 2500
 actions = 2
 
-DISCRETE_OS_SIZE = [20, 20]
-qtable = np.random.uniform(low=-2, high=0, size=(288, 512, 2))  # 285 is highest the pipe can be
 
-
-# def get_discrete_state(state):
-#     # discretestate = (state)/ discrete_os_win_size
-#     return qtable[state]
+q_table = np.random.uniform(low=-2, high=0, size=(288, 512, 2))  # 285 is highest the pipe can be
 
 
 for i in range(EPISODES):
-    action = np.random.randint(0, 2)
-    # np.argmax(qtable[:,discretestate])
-    # env.action_space.sample()
-    new_state, reward, done, _ = env.step(action)
-    env.render()
+    state = env.reset()
+    done = False
+    while not done:
+        action = np.argmax(q_table[state])
+        new_state, reward, done, _ = env.step(action)
+        env.render()
 
+        # if simulation didn't end after the previous step, update q-table
+        if not done:
 
+            max_future_q = np.max(q_table[new_state])
 
+            current_q = q_table[state + (action,)]   # current q-value
 
+            # new q-value using the Q-formula
+            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
 
+            q_table[state + (action,)] = new_q   # updates q-table with newly generated q-value
 
+        # Simulation ended (for any reason) - if goal position is achieved - update Q value with reward directly
+        # elif new_state[0] >= env.goal_position:
+            # q_table[state + (action,)] = 0
 
-
-
-
-    # new_discrete_state = get_discrete_state(new_state)
-    # if not done:
-    #     max_future_q = np.max(qtable[:,new_discrete_state])
-    #     currentq = qtable[:,(discretestate+(action,))]
-    #
-    #     new_q = (1-LEARNINGRATE)*currentq+LEARNINGRATE*(reward+DISCOUNT*max_future_q)
-    #     qtable[:,(discretestate+ (action,))] = new_q
-    #
-    # elif new_state[0] >= env.goal_position:
-    #     qtable[:,(discretestate+(action,))] = 0
-    #
-    # discretestate = new_discrete_state
+        state = new_state
 
 env.close()
 
